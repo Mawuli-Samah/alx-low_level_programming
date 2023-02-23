0e68069608777eb4c71bd5582ed7982c89de3bbe
Mawuli <125718648+Mawuli-Samah@users.noreply.github.com>
2023-02-23 04:55:46 -1200

    Update 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,5 +1,4 @@
 #include "main.h"
-
 /**
  * main - prints _putchar, followed by a new line
  * Return: Always 0 (Success)
@@ -14,6 +13,5 @@ int main(void)
     _putchar('h');
     _putchar('a');
     _putchat('r');
-
     return (0);
 }
