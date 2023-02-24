7574d6c65d22e22e6ecabf00586dcb92c45c4b04
Mawuli-Samah <Samahmawuli@gmail.com>
2023-02-24 06:12:27 -0800

    0x02. C - Functions, nested loops

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+  * print_alphabet - Make the alphabet
+  *
+  * Return: void
+  */
+void print_alphabet(void)
+{
+	char letter;
+
+	for (c = 'a'; c <= 'z'; letter++)
+	{
+		_putchar(letter);
+	}
+
+	_putchar('\n');
+}
+
+
