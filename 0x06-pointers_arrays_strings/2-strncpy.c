97c38ee1c50715cab4c15ccd04ac25b0f9f96c38
Mawuli <125718648+Mawuli-Samah@users.noreply.github.com>
2023-03-01 23:28:12 -1200

    Update 2-strncpy.c

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,15 +1,15 @@
 #include "main.h"
+
 /**
  * _strncpy - Copy a string
-  * @dest: The destination value
-  * @src: The source value
-  * @n: The copy limit
-  * * Return: char value
-  * 
+ * @dest: The destination value
+ * @src: The source value
+ * @n: The copy limit
+ * 
+ * Return: char value
  */
 char *_strncpy(char *dest, char *src, int n)
 {
-	
 int x;
 
 x = 0;
