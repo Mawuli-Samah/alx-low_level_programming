4972d5ff75bf460903ed57e4aca962e750fe1281
Mawuli-Samah <Samahmawuli@gmail.com>
2023-03-02 04:47:03 -0800

    0x06. C - More pointers, arrays and strings

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * leet - encodes a string into 1337
+ * @w: string to encode
+ *
+ * Return: address of s
+ */
+char *leet(char *w)
+{
+	int i, x;
+	char a[] = "aAeEoOtTlL";
+	char b[] = "4433007711";
+
+	for (i = 0; *(w + i); i++)
+	{
+		for (x = 0; x <= 9; x++)
+		{
+			if (a[x] == w[i])
+				w[i] = b[x];
+		}
+	}
+	return (w);
+}
