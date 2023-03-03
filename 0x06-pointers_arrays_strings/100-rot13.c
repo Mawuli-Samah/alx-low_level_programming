4b0e46862af6703fb8db471f7e922782aabe7fab
Mawuli <125718648+Mawuli-Samah@users.noreply.github.com>
2023-03-02 12:44:39 -1200

    Update 100-rot13.c

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -12,17 +12,17 @@ char *rot13(char *s)
 {
   int i;
   int j;
-  char good[] =
+  char data1[] =
   "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdfghijklmnopqrstuvwxyz";
-  char nice[] =
+  char datarot[] =
   "NOPQRSTUVWXYZABCDEFGHIJKLMnopqstuvwxyzabcdefghijklm";
   for (i = 0; s[i] != '\0'; i++)
   {
     for (j = 0; j < 52; j++)
     {
-      if (s[i] == good[j])
+      if (s[i] == data1[j])
       {
-        s[i] = nice[j];
+        s[i] = datarot[j];
         break;
       }
     }
